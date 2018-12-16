c64713d80b291ba5189ce4a0e5cb4c439bcdd3f1
unknown <reinsch.christoph@gmail.com>
2018-12-16 02:21:54 +0100

    Include newly released library by ac dimmer manufacturer: - dimming now works properly as expected - This library is not compatible with any other interrupts - So PinChangeInterrupt.h had to go -> Encoder wheel is now polling like encoder button - encoder speed and rotation persisting for UI now works properly

--- SunriseAlarmClock.ino
+++ SunriseAlarmClock.ino
@@ -23,25 +23,21 @@
 // P1 to pin 7 (DCF_POWER)
 // T to pin 2 (DCF_PIN)
 // G to GND, V to 3,3V ***MUST BE 3,3V***
-#include "PinChangeInterrupt.h"
-#include "DCF77.h"
-#include <RotaryEncoder.h>
-#include <TimerOne.h>
-#include <Time.h>
-#include <TimeLib.h>
-#include <OneButton.h>
-#include "ssd1306.h"
-#include "nano_gfx.h"
-
-#define DCF_PIN 2 // Connection pin to DCF 77 device
-#define DCF_INTERRUPT 0 // Interrupt number associated with pin
-#define DCF_POWER 7 // LOW - DCF on, HIGH - DCF off, do not leave dangling
+// #include "PinChangeInterrupt.h"
+// #include "DCF77.h"
+#include <RotaryEncoder.h> // https://github.com/mathertel/RotaryEncoder
+#include <RBDdimmer.h> // https://github.com/RobotDynOfficial/Lib-RBD-Dimmer-for-Mega-UNO-Leonardo
+#include <Time.h>      // https://github.com/PaulStoffregen/Time/blob/master/Time.cpp
+#include <TimeLib.h>   // https://github.com/PaulStoffregen/Time/blob/master/TimeLib.h
+#include <OneButton.h> // https://github.com/mathertel/OneButton
+#include "ssd1306.h"   // https://github.com/adafruit/Adafruit_SSD1306
+#include "nano_gfx.h"  // https://github.com/adafruit/Adafruit_SSD1306
 
 #define SW_PIN 13 // Pin of Encoder Button
 #define ENC_PIN_A 5 // Pin CLK of Encoder
 #define ENC_PIN_B 6 // Pin DT of Encoder
 
-#define AC_LOAD 11   // Output to Opto Triac pin, has to be 11 (Timer2)
+#define AC_LOAD 4   // Output to Opto Triac pin
 
 #define ENC_CW 1 // encoder rotating clockwise
 #define ENC_CCW -1 // encoder rotating counter-clockwise
@@ -53,7 +49,6 @@
 //////////// Internal (functionality) states
 boolean alarmEnabled = true; // if the alarm is primed
 boolean lightOn = false; // if the light is on
-boolean syncTimer1ToMains = false; // if the zeroCrossInterrupt should sync the Timer1
 
 //////////// UI states
 enum uiStates {
@@ -69,23 +64,20 @@ byte timeMinute = 0; // 0 - 59
 byte alarmHour = 6; // 0 - 23
 byte alarmMinute = 30; // 0 - 59
 byte alarmDuration = 15; // 0 - 30
-uint8_t lightIntensity = 128; // Dimming level (0-128)  0 = ON, 128 = OFF
+int8_t lightIntensity = 95; // Dimming level (5-95)  5 = OFF, 95 = ON
 
 //////////// Internal Variables
-uint16_t alarmIntensity = 0;
+uint16_t alarmIntensity = 1;
 uint8_t secondsPerStep;
 byte varToEdit = HOUR; // HOUR | MINUTE | DURATION
 
 //////////// Library variables
 time_t time;
-DCF77 DCF = DCF77(DCF_PIN, DCF_INTERRUPT);
 OneButton encoderBtn(SW_PIN, true);
 RotaryEncoder encoder(ENC_PIN_A, ENC_PIN_B);
+dimmerLamp dimmer(4); //initialise port for dimmer
 
 //////////// Loop Variables
-uint32_t encLastMillis; // last time read
-uint16_t encCount;      // running count of encoder clicks
-uint16_t encSpeed;      // last calculated speed in clicks/second
 uint8_t lastSecond;     // the last second any "set_View" was opened
 
 const PROGMEM uint8_t bell [] = {
@@ -98,83 +90,34 @@ const PROGMEM uint8_t empty [] = {
 };
 
 void setup() {
-  Serial.begin(57600);
+  Serial.begin(9600);
   Serial.println("Prototype software for a sunrise alarm clock/ dimmable bedside lamp");
   pinMode(SW_PIN, INPUT_PULLUP);
 
-  // Initialize DCF module
-  // pinMode(DCF_POWER, OUTPUT);
-  // digitalWrite(DCF_POWER, LOW);
-  // DCF.Start();
-
   // Initialize the dimming module
-  pinMode(AC_LOAD, OUTPUT);                    // Set AC Load pin as output
-  attachInterrupt(0, zeroCrossInt, RISING); // Choose the zero cross interrupt # from the table above
+  dimmer.begin(NORMAL_MODE, OFF);
+  dimmer.setPower(lightIntensity);
 
-  // set frequency for phase cutting to frequency of mains voltage
-  Timer1.initialize(10000);
-  Timer1.attachInterrupt(turnOnTriac);
-  Timer1.stop();
 
   // Initialize OLED screen
   ssd1306_128x64_i2c_init();
   ssd1306_fillScreen( 0x00 );
   ssd1306_setFixedFont(ssd1306xled_font6x8);
 
-  // Initialize pwm output
-  // pinMode(PWM_PIN, OUTPUT);
-  // digitalWrite(PWM_PIN, LOW);
-
   // Initialize OneButton.h functionality for encoder button
   encoderBtn.attachClick(toggleLightOn);
   encoderBtn.attachDoubleClick(toggleAlarmEnabled);
   encoderBtn.attachLongPressStop(switchToSetAlarmView);
 
-  // Initialize encoder wheel
-  pinMode(ENC_PIN_A, INPUT);
-  pinMode(ENC_PIN_B, INPUT);
-  attachPCINT(digitalPinToPCINT(5), encoderInterrupt, CHANGE);
-  attachPCINT(digitalPinToPCINT(6), encoderInterrupt, CHANGE);
-
   secondsPerStep = alarmDuration * 60 / 255;
-}
-
-void zeroCrossInt() { // function to be fired at the zero crossing to dim the light
-  // Firing angle calculation : 1 full 50Hz wave =1/50=20ms
-  // Every zerocrossing thus: (50Hz)-> 10ms (1/2 Cycle) For 60Hz => 8.33ms (10.000/120)
-  // 10ms=10000us
-  // (10000us - 10us) / 128 = 75 (Approx) For 60Hz =>65
-  if (syncTimer1ToMains) {
-    syncTimer1ToMains = false;
-    delayMicroseconds(75 * lightIntensity); // Off cycle
-    digitalWrite(AC_LOAD, HIGH);         // triac firing
-    Timer1.start();
-    delayMicroseconds(10);               // triac On propogation delay (for 60Hz use 8.33)
-    // digitalWrite(AC_LOAD, LOW);          // triac Off
-  }
-  digitalWrite(AC_LOAD, LOW); // triac Off 
-}
-
-/**
- * Interrupt Function to be fired for Timer1
- */
-void turnOnTriac() {
-  if (lightOn) {
-    digitalWrite(AC_LOAD, HIGH);
-  }
 }
 
 /**
    A single click function of the encoder button
 */
 void toggleLightOn () {
-  // if (lightOn) {
-  //   analogWrite(PWM_PIN, 0);
-  // } else {
-  //   analogWrite(PWM_PIN, lightIntensity);
-  // }
   lightOn = !lightOn;
-  syncTimer1ToMains = true;
+  dimmer.changeState();
 }
 
 /**
@@ -209,8 +152,6 @@ void switchToSetTimeView() {
   timeHour = hour();
   timeMinute = minute();
   uiState = setTimeView;
-  // setAlarm = false;
-  // timeState = true;
   encoderBtn.attachClick(cycleSetTime);
   encoderBtn.attachDoubleClick(switchToSetAlarmView);
   encoderBtn.attachLongPressStop(switchToMainView);
@@ -224,8 +165,6 @@ void switchToSetTimeView() {
 void switchToSetAlarmView () {
   ssd1306_clearScreen();
   uiState = setAlarmView;
-  // timeState = false;
-  // setAlarm = true;
   encoderBtn.attachClick(cycleSetAlarm);
   encoderBtn.attachDoubleClick(switchToSetTimeView);
   encoderBtn.attachLongPressStop(switchToMainView);
@@ -241,59 +180,35 @@ void switchToMainView () {
     setTime(timeHour, timeMinute, 0, 14, 3, 15);
   }
   uiState = mainView;
-  // timeState = false;
-  // setAlarm = false;
   varToEdit = HOUR;
   encoderBtn.attachClick(toggleLightOn);
   encoderBtn.attachDoubleClick(toggleAlarmEnabled);
   encoderBtn.attachLongPressStop(switchToSetAlarmView);
 }
 
-/**
-   The interrupt function for the encoder wheel
-*/
-void encoderInterrupt () {
-  encoder.tick(); // just call tick() to check the state.
-  encCount++;
-  if (millis() - encLastMillis > 500) {
-    encSpeed = encCount * (0.25);
-    encLastMillis = millis();
-    encCount = 0;
-  }
-}
-
 void loop() {
-  uint32_t lastMillis;     // last time read
-  uint8_t persistRotation; // count the number off loops to persist the last turned direction for the UI
-  int8_t encDir = encoder.getDirection();
+  static uint32_t encLastMillis;         // last time read
+  static uint16_t encSpeed;              // last calculated speed in clicks/second
+  static uint32_t lastMillis;     // last time read
   encoderBtn.tick(); // check state of encoder Button
-  
+  encoder.tick();
+  int8_t encDir = encoder.getDirection();
+
   // Persist the last direction so the UI doesn't flash rapidly when the encoder is turned,
   // but the variable to set isn't displayed because it's an odd second
   if (encDir) {
-    persistRotation = 50;
-  } else {
-    persistRotation = persistRotation > 0 ? persistRotation-- : 0;
+    // also calculate the speed of the encoder
+    // 150 is just my prefence for dimming speed control
+    encSpeed = (1.0 / (millis() - encLastMillis)) * 150;
+    encLastMillis = millis();
   }
-
-  // time_t DCFtime = DCF.getTime(); // is there an new DCF77 time
-  // if (DCFtime != 0)
-  // {
-  //   setTime(DCFtime);
-  // }
 
   if (lightOn && encDir && uiState == mainView)
   {
     // change brightness of light when on main view
-    if (encDir == ENC_CW) {
-      lightIntensity = (lightIntensity > 120 - encSpeed ? 
-                        120 : lightIntensity + encSpeed);
-    } else if (encDir == ENC_CCW) {
-      lightIntensity = (lightIntensity < encSpeed ? 
-                        5 : lightIntensity - encSpeed);
-    }
-    syncTimer1ToMains = true;
-    // analogWrite(PWM_PIN, lightIntensity);
+    lightIntensity += encDir * encSpeed;
+    if (lightIntensity > 95) lightIntensity = 95; else if (lightIntensity < 5) lightIntensity = 5;
+    dimmer.setPower(lightIntensity);
   }
 
   if (uiState == setAlarmView) { // handle changing of the alarm time
@@ -324,31 +239,29 @@ void loop() {
 
   if (alarmEnabled && isAlarmOn()) {
     // do alarm handling here (incrementing alarmIntensity)
-    if (alarmIntensity == 255) { //Alarm ist zu Ende
+    if (alarmIntensity == 95) { //Alarm ist zu Ende
       lightOn = true;
-      lightIntensity = 255;
+      lightIntensity = 95;
       alarmIntensity = 0;
     }
   }
 
   // UI stuff
   if (millis() - lastMillis > 250) { //draw 4 frames per second
+    bool rotated = (millis() - encLastMillis) < 500;
     if (uiState != mainView && ((second() - lastSecond) + 60) % 60 > 15) {
       // switch back to main view after 15 seconds of idling in timeState or setAlarm
       switchToMainView(); 
     }
     if (uiState == setAlarmView) {
-      drawSetAlarmView(persistRotation > 0);
+      drawSetAlarmView(rotated);
     } else if (uiState == setTimeView) {
-      drawSetTimeView(persistRotation > 0);
+      drawSetTimeView(rotated);
     } else {
       drawMainView();
     }
     drawHeader();
     lastMillis = millis();
-    if (lightOn) {
-      syncTimer1ToMains = true;
-    }
   }
 }
 
@@ -396,18 +309,19 @@ void drawMainView() {
   }
   ssd1306_printFixedN(0, 36, digits, STYLE_NORMAL, FONT_SIZE_4X);
 
-  // // Draw the lightIntensity
+  // Draw the lightIntensity
   // uint8_t rectWidth = 4;
   // uint8_t rectHeight = 48;
-  // uint8_t intensityRectHeight = rectHeight * (lightIntensity / 255);
+  // uint8_t intensityRectHeight = rectHeight * (lightIntensity / 100);
   // uint8_t intensityRectOffsetY = rectHeight - intensityRectHeight;
   // uint8_t buffer[rectWidth * rectHeight / 8];
   // NanoCanvas canvas(rectWidth, rectHeight, buffer);
   // canvas.clear();
   // // canvas.setOffset(intensityRectOffsetY, 0);
-  // canvas.fillRect(124, 16, 127, 16 + intensityRectHeight, 0xFF);
+  // canvas.fillRect(124, 16, 4, 16 + intensityRectHeight, 0xFF);
   // canvas.blt((ssd1306_displayWidth() - 80), 16);
 }
+
 /**
  * Draw the changing of the alarm time
  */
