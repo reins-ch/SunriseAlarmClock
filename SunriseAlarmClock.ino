3196616e324e3b3a6e8567ff1717a18e5fc0be04
unknown <reinsch.christoph@gmail.com>
2018-12-18 22:04:41 +0100

    Implemented working sunrise

--- SunriseAlarmClock.ino
+++ SunriseAlarmClock.ino
@@ -43,6 +43,13 @@
 #define MINUTE 2 // enum for setting time
 #define DURATION 3 // enum for setting time
 
+#define MINUTESPERHOUR 60
+#define HOURSPERDAY 24
+#define MAXDURATION 11 // maximum duration of the alarm
+
+// 150 is just my prefence for dimming speed control
+#define ENCODERSPEED 150
+
 //////////// Internal (functionality) states
 boolean alarmEnabled = true; // if the alarm is primed
 boolean lightOn = false; // if the light is on
@@ -58,21 +65,22 @@ uiStates uiState = mainView;
 //////////// Variables being changed by the user
 int8_t timeHour = 0; // 0 - 23
 int8_t timeMinute = 0; // 0 - 59
-int8_t alarmHour = 6; // 0 - 23
-int8_t alarmMinute = 30; // 0 - 59
-int8_t alarmDuration = 15; // 0 - 30
+int8_t alarmHour = 0; // 0 - 23
+int8_t alarmMinute = 2; // 0 - 59
+int8_t alarmDuration = 2; // 0 - 10
 int8_t lightIntensity = 95; // Dimming level (5-95)  5 = OFF, 95 = ON
 
 //////////// Internal Variables
-uint16_t alarmIntensity = 1;
-uint8_t secondsPerStep;
+// store the dimming values for every 10 seconds
+uint8_t alarmValues[MAXDURATION*6];
+uint8_t alarmValueIter = 0;
 byte varToEdit = HOUR; // HOUR | MINUTE | DURATION
 
 //////////// Library variables
 time_t time;
 OneButton encoderBtn(SW_PIN, true);
 RotaryEncoder encoder(ENC_PIN_A, ENC_PIN_B);
-dimmerLamp dimmer(4); //initialise port for dimmer
+dimmerLamp dimmer(AC_LOAD); //initialise port for dimmer
 
 //////////// Loop Variables
 uint8_t lastSecond;     // the last second any "set_View" was opened
@@ -106,7 +114,10 @@ void setup() {
   encoderBtn.attachDoubleClick(toggleAlarmEnabled);
   encoderBtn.attachLongPressStop(switchToSetAlarmView);
 
-  secondsPerStep = alarmDuration * 60 / 255;
+  // calculate the dimming value every 10 seconds
+  for (int i = 0; i < alarmDuration*6; i++) {
+    alarmValues[i] = dimValue(i);
+  }
 }
 
 /**
@@ -135,7 +146,6 @@ void cycleSetAlarm() {
 /**
    A single click function of the encoder button
 */
-
 void cycleSetTime() {
   varToEdit = (varToEdit == MINUTE ? HOUR : varToEdit + 1);
   lastSecond = second();
@@ -176,6 +186,12 @@ void switchToMainView () {
   if (uiState == setTimeView) {
     setTime(timeHour, timeMinute, 0, 14, 3, 15);
   }
+  if (uiState == setAlarmView) {
+    // recalculate alarmValues array
+    for (int i = 0; i < alarmDuration * 6; i++) {
+      alarmValues[i] = dimValue(i);
+    }
+  }
   uiState = mainView;
   varToEdit = HOUR;
   encoderBtn.attachClick(toggleLightOn);
@@ -187,6 +203,8 @@ void loop() {
   static uint32_t encLastMillis;         // last time read
   static uint16_t encSpeed;              // last calculated speed in clicks/second
   static uint32_t lastMillis;     // last time read
+  static int8_t lastAlarmCheckSecond;
+  int8_t loopSecond = second();
   encoderBtn.tick(); // check state of encoder Button
   encoder.tick();
   int8_t encDir = encoder.getDirection();
@@ -195,13 +213,11 @@ void loop() {
   // but the variable to set isn't displayed because it's an odd second
   if (encDir) {
     // also calculate the speed of the encoder
-    // 150 is just my prefence for dimming speed control
-    encSpeed = (1.0 / (millis() - encLastMillis)) * 150;
+    encSpeed = (1.0 / (millis() - encLastMillis)) * ENCODERSPEED;
     encLastMillis = millis();
   }
 
-  if (lightOn && encDir && uiState == mainView)
-  {
+  if (lightOn && encDir && uiState == mainView) {
     // change brightness of light when on main view
     lightIntensity += encDir * encSpeed;
     if (lightIntensity > 95) lightIntensity = 95; else if (lightIntensity < 5) lightIntensity = 5;
@@ -211,14 +227,13 @@ void loop() {
   if (uiState == setAlarmView) { // handle changing of the alarm time
     switch (varToEdit) {
       case HOUR:
-        handleAlarmTime(encDir, alarmHour, 23);
+        handleAlarmTime(encDir, alarmHour, HOURSPERDAY);
         break;
       case MINUTE:
-        handleAlarmTime(encDir, alarmMinute, 59);
+        handleAlarmTime(encDir, alarmMinute, MINUTESPERHOUR);
         break;
       case DURATION:
-        handleAlarmTime(encDir, alarmDuration, 30);
-        secondsPerStep = alarmDuration * 60 / 255;
+        handleAlarmTime(encDir, alarmDuration, MAXDURATION);
         break;
     }
   }
@@ -226,36 +241,54 @@ void loop() {
   if (uiState == setTimeView) { // handle changing of the time
     switch (varToEdit) {
       case HOUR:
-        handleAlarmTime(encDir, timeHour, 23);
+        handleAlarmTime(encDir, timeHour, HOURSPERDAY);
         break;
       case MINUTE:
-        handleAlarmTime(encDir, timeMinute, 59);
+        handleAlarmTime(encDir, timeMinute, MINUTESPERHOUR);
         break;
     }
   }
 
-  if (alarmEnabled && isAlarmOn()) {
-    // do alarm handling here (incrementing alarmIntensity)
-    if (alarmIntensity == 95) { //Alarm ist zu Ende
+  if ((loopSecond - lastAlarmCheckSecond + SECS_PER_MIN) % 60 > 10 && alarmEnabled) {
+    // handle light increasing if alarm is on
+    static bool alarmOn;
+    lastAlarmCheckSecond = loopSecond;
+    if (alarmOn) {
+      lightIntensity = alarmValues[alarmValueIter];
+      dimmer.setPower(lightIntensity);
+      alarmValueIter++;
+      if (alarmValueIter > alarmDuration * 6) { // alarm is finished
+        alarmOn = false;
+        alarmValueIter = 0;
+        dimmer.setPower(95);
+      }
+    } else if (isAlarmOn()) { // alarm just went on
+      alarmOn = true;
       lightOn = true;
-      lightIntensity = 95;
-      alarmIntensity = 0;
+      lightIntensity = alarmValues[alarmValueIter];
+      alarmValueIter++;
+      dimmer.setPower(lightIntensity);
+      dimmer.setState(ON);
     }
   }
 
   // UI stuff
   if (millis() - lastMillis > 250) { //draw 4 frames per second
     bool rotated = (millis() - encLastMillis) < 500;
-    if (uiState != mainView && ((second() - lastSecond) + 60) % 60 > 15) {
+    if (rotated && uiState != mainView) {
+      // delay automatic switching to main view if rotated
+      lastSecond = loopSecond;
+    }
+    if (uiState != mainView && ((loopSecond - lastSecond) + 60) % 60 > 15) {
       // switch back to main view after 15 seconds of idling in timeState or setAlarm
       switchToMainView(); 
     }
     if (uiState == setAlarmView) {
-      drawSetAlarmView(rotated);
+      drawSetAlarmView(loopSecond, rotated);
     } else if (uiState == setTimeView) {
-      drawSetTimeView(rotated);
+      drawSetTimeView(loopSecond, rotated);
     } else {
-      drawMainView();
+      drawMainView(loopSecond);
     }
     drawHeader();
     lastMillis = millis();
@@ -295,11 +328,11 @@ void drawHeader() {
 /**
  * Draw what should be displayed when you're not interacting with the device
  */
-void drawMainView() {
+void drawMainView(int8_t loopSecond) {
   char digits[5];
 
   // Draw the current time
-  if (second() % 2) {
+  if (loopSecond % 2) {
     sprintf(digits, "%02d:%02d", hour(), minute());
   } else {
     sprintf(digits, "%02d %02d", hour(), minute());
@@ -322,10 +355,10 @@ void drawMainView() {
 /**
  * Draw the changing of the alarm time
  */
-void drawSetAlarmView(boolean rotated) {
+void drawSetAlarmView(int8_t loopSecond, boolean rotated) {
   char digits[12];
   sprintf(digits, "%02d:%02d%02d'    ", alarmHour, alarmMinute, alarmDuration);
-  if (second() % 2 && !rotated) {
+  if (loopSecond % 2 && !rotated) {
     switch (varToEdit) {
       case HOUR:
         digits[0] = ' ';
@@ -352,10 +385,10 @@ void drawSetAlarmView(boolean rotated) {
 /**
  * Draw the changing of the clock time
  */
-void drawSetTimeView(boolean rotated) {
+void drawSetTimeView(int8_t loopSecond, boolean rotated) {
   char digits[5];
   sprintf(digits, "%02d:%02d", timeHour, timeMinute);
-  if (second() % 2 && !rotated) {
+  if (loopSecond % 2 && !rotated) {
     switch (varToEdit) {
       case HOUR:
         digits[0] = ' ';
@@ -374,11 +407,11 @@ void drawSetTimeView(boolean rotated) {
    Handles the functionality of changing a unit of the alarm
    @param encDir -1 || 0 || +1
    @param &var the variable to change (alarmHour, alarmMinute, alarmDuration)
-   @param maxNumber 23 for hour, 59 for minute, maybe 30 for duration
+   @param maxNumber 24 for hour, 60 for minute, maybe 10 for duration
 */
 void handleAlarmTime(int8_t encDir, int8_t &var, byte maxNumber) {
   var += encDir;
-  if (var == maxNumber + 1)
+  if (var == maxNumber)
     var = 0;
   else if (var == -1)
     var = maxNumber;
@@ -388,15 +421,14 @@ void handleAlarmTime(int8_t encDir, int8_t &var, byte maxNumber) {
    Checks whether the rising alarm should be on at the moment
 */
 boolean isAlarmOn() {
-  if (alarmMinute >= alarmDuration) { 
-    // minute handle doesn't pass 0 in the duration
-    return (hour() == alarmHour
-            && minute() >= (alarmMinute - alarmDuration)
-            && minute() < alarmMinute);
-  } else {
-    byte startHour = alarmHour == 0 ? 23 : (alarmHour - 1);
-    byte startMinute = 60 - (alarmDuration - alarmMinute);
-    return ((hour() == startHour && minute() >= startMinute)
-            || (hour() == alarmHour && minute() < alarmMinute));
-  }
+  return ((alarmHour - hour() + HOURSPERDAY) % HOURSPERDAY <= 1) && 
+    ((alarmMinute - minute() + MINUTESPERHOUR) % MINUTESPERHOUR < alarmDuration);
+}
+
+/**
+ * Calculate the dimming value for a time step
+ * @param i the time step
+ */
+int8_t dimValue(int i) {
+  return (int) 5 * pow(pow(19.0, 1.0/(alarmDuration*6)), i);
 }
